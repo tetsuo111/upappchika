507c0fd529d3211d0c85cb9f574045d2367cc3de
tech-camp-019 <tech-camp-019@tech-camp-019-no-MacBook-Air.local>
2015-06-21 20:17:25 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+
+User.create(:email => 'tech-camp@gmail.com',:password => '12345678',:nickname => "Mako")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
