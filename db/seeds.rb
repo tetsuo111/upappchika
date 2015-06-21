80e34a12fef713bf60cc29dc6f1e5154b1b429b7
tech-camp-019 <tech-camp-019@tech-camp-019-no-MacBook-Air.local>
2015-06-21 20:26:40 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-User.create(:email => 'tech-camp@gmail.com',:password => '12345678',:nickname => "Mako")
+Test.create(:title => 'great')
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,3 +7,12 @@ User.create(:email => 'tech-camp@gmail.com',:password => '12345678',:nickname =>
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require "csv"
+ 
+companies_csv = CSV.readlines("db/companies.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Company.create(:name => row[1], :url => row[2], :map => row[3])
+end
