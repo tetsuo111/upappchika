27085ad495c7461574ea68008e41374f9b961a36
tech-camp-019 <tech-camp-019@tech-camp-019-no-MacBook-Air.local>
2015-06-22 12:50:44 +0900

    update design

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621095322) do
+ActiveRecord::Schema.define(version: 20150622034705) do
 
   create_table "tests", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "text",       limit: 65535
+    t.text     "image",      limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
